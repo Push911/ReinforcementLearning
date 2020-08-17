7c36c6d79985f79dd74a6e6f4e446fdbacb3b653
Push911 <49253201+Push911@users.noreply.github.com>
2020-08-17 17:58:21 +0200

    Rewrited as class. Added descriptions.

--- Q-Learning.py
+++ Q-Learning.py
@@ -4,6 +4,7 @@ import numpy as np
 from support import plotting
 from support.windy_gridworld import WindyGridworldEnv
 import matplotlib.style
+
 matplotlib.style.use("seaborn")
 
 """ Q-Learning is a basic form of Reinforcement Learning which uses Q-values (also called action values) to iteratively 
@@ -19,8 +20,8 @@ matplotlib.style.use("seaborn")
 3.  Temporal Difference or TD-Update:
         The Temporal Difference or TD-Update rule can be represented as follows :
         Q(S, A) <- Q(S, A) + alpha(R + gamma * Q(S', A') - Q(S, A)
-        
-        
+
+
 This update rule to estimate the value of Q is applied at every time step of the agents interaction with the 
 environment. The terms used are:
 
@@ -45,57 +46,61 @@ It goes as follows : ~With probability (1-epsilon) choose the action which has t
                 DOWN = 2
                 LEFT = 3"""
 
-env = WindyGridworldEnv()
-# env.seed(100)
-# print(env.render())
 
+class QLearning:
+    def __init__(self):
+        self.env = WindyGridworldEnv()
+        # self.env.render()
+        self.QFunction = defaultdict(lambda: np.zeros(self.env.action_space.n))
+        self.epsilon = 0.1
+        self.episodesAmount = 1000
+        self.stats = plotting.EpisodeStats(episode_lengths=np.zeros(self.episodesAmount),
+                                           episode_rewards=np.zeros(self.episodesAmount))
+        self.actionProbabilities = np.ones(self.env.action_space.n, dtype=float)
+        self.qLearning()
+        plotting.plot_episode_stats(self.stats)
 
-def createEpsilonGreedyPolicy(QFunction, epsilon, actionsAmount):
     # Creates an epsilon-greedy policy based on a given Q-function and epsilon
-    def policyFunction(state):
+    def createEpsilonGreedyPolicy(self):
         # state - represent current position in array (linear)
-        actionProbabilities = np.ones(actionsAmount, dtype=float) * epsilon / actionsAmount
-        # QFunction[state] - represents the next move direction probabilities for each direction
-        bestAction = np.argmax(QFunction[state])
-        # bestAction - direction to move,
-        actionProbabilities[bestAction] += (1.0 - epsilon)
-        return actionProbabilities
-    return policyFunction
+        def policyFunction(state):
+            # actionProbabilities - represents the next move direction probabilities for each direction
+            self.actionProbabilities = np.ones(self.env.action_space.n,
+                                               dtype=float) * self.epsilon / self.env.action_space.n
+            # QFunction[state] - represents the next move direction probabilities for each direction
+            bestAction = np.argmax(self.QFunction[state])
+            # bestAction - direction to move
+            self.actionProbabilities[bestAction] += (1.0 - self.epsilon)
+        return policyFunction
+
+    def qLearning(self, discountFactor=1.0, alpha=0.6):
+        policy = self.createEpsilonGreedyPolicy()
+
+        for episode in range(self.episodesAmount):
+            state = self.env.reset()
+            for i in itertools.count():
+                policy(state)
+                # action - random choice from actionProbabilities 1D array with probability p
+                action = np.random.choice(np.arange(len(self.actionProbabilities)), p=self.actionProbabilities)
+                # print(env.render())
+                # nextState - current position on the grid,
+                # reward - -1.0 perm,
+                # done - boolean, to show if agent reached destination (won),
+                # last(skipped) parameter is probability(1.0 by default)
+                nextState, reward, done, _ = self.env.step(action)
+
+                self.stats.episode_rewards[episode] += reward
+                self.stats.episode_lengths[episode] = i
+                bestNextAction = np.argmax(self.QFunction[nextState])
+                # temporalDifferenceTarget - reward(-1.0) + best action from the next state
+                temporalDifferenceTarget = reward + discountFactor * self.QFunction[nextState][bestNextAction]
+                # temporalDifferenceDelta - temporalDifferenceTarget - action from current state
+                temporalDifferenceDelta = temporalDifferenceTarget - self.QFunction[state][action]
+                self.QFunction[state][action] += alpha * temporalDifferenceDelta
+                if done:
+                    break
+                # print(env.render())
+                state = nextState
 
 
-def qLearning(environment, episodesAmount, discountFactor=1.0, alpha=0.6, epsilon=0.1):
-    QFunction = defaultdict(lambda: np.zeros(environment.action_space.n))
-    stats = plotting.EpisodeStats(episode_lengths=np.zeros(episodesAmount), episode_rewards=np.zeros(episodesAmount))
-    policy = createEpsilonGreedyPolicy(QFunction, epsilon, environment.action_space.n)
-
-    for episode in range(episodesAmount):
-        state = environment.reset()
-        for i in itertools.count():
-            actionProbabilities = policy(state)
-            print(actionProbabilities)
-    #         # actionProbabilities - represents the next move direction probabilities for each direction
-    #         action = np.random.choice(np.arange(len(actionProbabilities)), p=actionProbabilities)
-    #         # action - random choice from actionProbabilities 1D array
-    #         nextState, reward, done, _ = environment.step(action)
-    #         # print(env.render())
-    #         # nextState - current position on the grid,
-    #         # reward - ,
-    #         # done - boolean, to show if agent reached destination(won)
-    #         stats.episode_rewards[episode] += reward
-    #         stats.episode_lengths[episode] = i
-    #         bestNextAction = np.argmax(QFunction[nextState])
-    #         # print("rew", reward)
-    #         temporalDifferenceTarget = reward + discountFactor * QFunction[nextState][bestNextAction]
-    #         print(temporalDifferenceTarget)
-    #         temporalDifferenceDelta = temporalDifferenceTarget - QFunction[state][action]
-    #         QFunction[state][action] += alpha * temporalDifferenceDelta
-    #         if done:
-    #             break
-    #         # print(env.render())
-    #         state = nextState
-    #
-    # return QFunction, stats
-
-
-Q, stats = qLearning(env, 1000)
-plotting.plot_episode_stats(stats)
+QLearning()
